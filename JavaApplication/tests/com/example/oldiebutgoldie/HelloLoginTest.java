8940e1217499efae2c310401a87ba9676409f22e
SamuelAhlvin <ahlvinsamme@gmail.com>
2022-04-27 12:50:33 +0200

    Added tests directory and test classes

---
+++ JavaApplication/tests/com/example/oldiebutgoldie/HelloLoginTest.java
@@ -0,0 +1,22 @@
+package com.example.oldiebutgoldie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloLoginTest {
+
+    @BeforeEach
+    void setUp() {
+        HelloLogin test = new HelloLogin();
+    }
+
+    @Test
+    void start() {
+    }
+}
